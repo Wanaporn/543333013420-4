b8bfdbda95aaa4d9c6b3d6a6f6fead60758600e2
ADMIN <ADMIN@BowBo>
2014-04-22 13:52:19 +0700

    addFirst , addLast

---
+++ src/com/rmuti/datastructure/TestArrayList.java
@@ -0,0 +1,40 @@
+package com.rmuti.datastructure;
+
+public class TestArrayList {
+
+	public static void main(String[] args) {
+		System.out.println("ArrayList");
+		 		ArrayList array = new ArrayList();
+		 		array.add("1");
+		 		System.out.println(array);
+		 		array.add("2");
+		 		System.out.println(array);
+		 		array.add("3");
+		 		System.out.println(array);
+		 		array.add("4");
+		 		System.out.println(array);
+		 		array.add("5");
+		 		System.out.println(array);
+		 		
+		 		System.out.println("First");
+		 		
+		 		array.addFirst(0);
+		 		System.out.println("Add.First = "+array);
+		 		
+		 		System.out.println("Last");
+		 		
+		 		array.addLast(4);
+				System.out.println("Add.Last = "+array);
+		 		
+		 		System.out.println("Size");
+		 		
+		 		array.size();
+		 		System.out.println("Size = "+array.size());
+		 		
+		 		System.out.println("Remove Array 4");
+		 		
+		 		array.remove(4);
+		 		System.out.println("Remove = "+array);
+		 	}
+		 
+} 
